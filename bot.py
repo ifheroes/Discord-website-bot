d7d1de3e6c48e6bd7488b332ff6185dbebfceadc
chrischivlog <coneisz@chrischi.one>
2021-08-16 18:27:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
-# Imports
 import discord
-from discord.ext import commands
 
-# Credentials
-TOKEN = ''
+class MyClient(discord.Client):
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('')
