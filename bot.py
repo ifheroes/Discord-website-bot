c4b8c1b5bb7ef5fe1272dd0a8c38d15fa81a4721
Luca <63116530+EckigerLuca@users.noreply.github.com>
2021-08-30 20:57:14 +0200

    Added gitignore and changed structure in main.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-import discord
-
-class MyClient(discord.Client):
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-
-client = MyClient()
-client.run('')
---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import json
+import os
+
+with open("config.json", "r") as f:
+    data = json.load(f)
+TOKEN = data["token"]
+
+client = commands.Bot(command_prefix='if!', intents = discord.Intents.default(), case_insensitive=True)
+
+@client.event()
+async def on_ready():
+    print('\nReady!\n')
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py") and filename != "__init__.py":
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+
+client.run(TOKEN)
