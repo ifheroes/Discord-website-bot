cb94bfc1cea9b78ef65afdb2ad352c5ab2dc8f2b
Luca <63116530+EckigerLuca@users.noreply.github.com>
2021-09-01 19:38:26 +0200

    text splitting/filtering

--- cogs/listener.py
+++ cogs/listener.py
@@ -14,7 +14,22 @@ class listener(commands.Cog):
                 data = json.load(f)
             channel_id = data["channel_id"]
             if message.channel.id == int(channel_id):
-                print(message.content)
+                if message.attachments != [] and str(message.attachments[0]).endswith(('.png', '.jpg', '.jpeg')):
+                    if message.content.startswith('```') and message.content.endswith('```') and 'Headline:' in str(message.content) and 'Body:' in str(message.content):
+                        message_raw = message.content[3:-3]
+                        message_head = message_raw.split('Body:')[0].strip().split('Headline:')[1][1:]
+
+                        message_body = message_raw.split('Headline:')[1].split('Body')[1][2:]
+
+                        data = {"headline":message_head, "body":message_body, "img_url": message.attachments[0].url}
+                        y = json.dumps(data, indent=4)
+                        print(y)
+                    else:
+                        await message.add_reaction('❌')
+                        return
+                else:
+                    await message.add_reaction('❌')
+                    return
 
     
 
